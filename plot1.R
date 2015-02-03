c0c20fa18c0281413eeabc62857af77acd2f4de7
erpreciso <trepreciso@gmail.com>
2015-02-03 21:34:21 +0100

    create first script

--- plot1.R
+++ plot1.R
@@ -1,3 +1,19 @@
-setwd("/home/erpreciso/Documents/school")
-data <- read.table(unz("exdata_data_household_power_consumption.zip", "household_power_consumption.txt"), header = TRUE, sep = ";")
-data$Date <- strptime(data$Date, "%d/%m/%Y")
+
+# read data
+data <- read.table(unz("exdata_data_household_power_consumption.zip", "household_power_consumption.txt"), header = TRUE, sep = ";", stringsAsFactors = FALSE)
+# convert date and time in timestamp
+data$timestamp <- strptime(paste(data[,"Date"],data[, "Time"], " "), "%d/%m/%Y %H:%M:%S")
+# set date limits for the subset
+ub <- strptime("2007-02-02 23:59:59", "%Y-%m-%d %H:%M:%S")
+lb <- strptime("2007-02-01 00:00:00", "%Y-%m-%d %H:%M:%S")
+# subset data
+ds <- data[data$timestamp > lb & data$timestamp < ub,]
+# select data to plot
+ap <- as.numeric(ds$Global_active_power)
+# create device
+png(file = "out/plot1.png", bg = "transparent")
+# create histogram
+hist(ap, breaks = 20, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+# close device
+dev.off()
+
